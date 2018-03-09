9076f458449a88062294d1d1ac223c0e0b3473fe
Paul Ehrhardt <paul.ehrhardt@hec.edu>
2018-03-09 16:11:22 +0100

    bootstrap forms

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,6 +5,7 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
   def new
@@ -14,6 +15,12 @@ class CocktailsController < ApplicationController
   def create
     cocktail = Cocktail.create(set_params)
     redirect_to cocktail_path(cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
   end
 
   private
